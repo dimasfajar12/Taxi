a5532a51be072005eeb4b25a7b46b9533ccda9fe
dimasfajar12 <80519340+dimasfajar12@users.noreply.github.com>
2021-03-18 11:31:24 +0700

    Add files via upload

---
+++ taxi02/taksi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi02
+{
+    class taksi
+    {
+
+        public string namadriver { get; set; }
+        public bool sedangbertugas { get; set; }
+        public int penumpang { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine(" Nama Driver     : {0} ", namadriver);
+            if (sedangbertugas == true)
+            {
+                Console.WriteLine(" Sedang mengantar penumpang ", sedangbertugas);
+            }
+            else
+            {
+                Console.WriteLine(" Sedang tidak mengantar penumpang ", sedangbertugas);
+            }
+
+            Console.WriteLine(" Jumlah penumpang  : {0}\n ", penumpang);
+        }
+
+        public void angkutpenumpang()
+        {
+            Console.WriteLine(" {0} Sedang menjemput penumpang ", namadriver);
+        }
+
+        public void turunpenumpang()
+        {
+            Console.WriteLine(" {0} Selesai menjemput penumpang\n ", namadriver);
+        }
+    }
+}
